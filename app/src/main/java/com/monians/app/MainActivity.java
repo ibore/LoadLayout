2334930c0074ed969fe660ef06c18db1a213cbb9
ibore <bore521@live.com>
2016-06-07 19:24:13 +0800

    修改了Demo

--- app/src/main/java/com/monians/app/MainActivity.java
+++ app/src/main/java/com/monians/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.monians.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -9,11 +10,28 @@ import com.monians.xload.XLoadLayout;
 
 public class MainActivity extends AppCompatActivity implements XLoadLayout.OnLoadClickListener{
 
+    private LayoutInflater inflater;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inflater = LayoutInflater.from(this);
+
         final XLoadLayout xLoadLayout = (XLoadLayout) findViewById(R.id.xloadlayout);
+        // 通过代码添加
+//        xLoadLayout.setLoadingView(R.layout.layout_loading);
+//        xLoadLayout.setEmptyView(R.layout.layout_empty);
+//        xLoadLayout.setErrorView(R.layout.layout_error);
+
+//        View loadingView = inflater.inflate(R.layout.layout_loading, null);
+//        xLoadLayout.setLoadingView(loadingView);
+//        View emptyView = inflater.inflate(R.layout.layout_empty, null);
+//        xLoadLayout.setEmptyView(emptyView);
+//        View errorView = inflater.inflate(R.layout.layout_empty, null);
+//        xLoadLayout.setErrorView(errorView);
+
+
         findViewById(R.id.loading).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +68,9 @@ public class MainActivity extends AppCompatActivity implements XLoadLayout.OnLoa
                 Toast.makeText(getApplicationContext(), "点击重试", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
     }
 
     @Override
